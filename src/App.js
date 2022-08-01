e8935e944537fcf62f54f8ff9851f2b4b4c7df33
limjeahyuk <dlawo9368@email.daelim.ac.kr>
2022-08-01 14:36:13 +0900

    routes-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
+import Main from './pages/main/Main';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Main />} />
+      </Routes>
     </div>
   );
 }
