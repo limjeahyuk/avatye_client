fa518339e8a5cf9ec9bf3714de4005ba39fa7684
Jsubak <tnwjd1003@gmail.com>
2022-08-05 13:53:14 +0900

    프로필 및 탭추가

---
+++ src/components/user/Tabs/MyTabs.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+import ProfileTab from './Tab/ProfileTab';
+import SupportProject from './Tab/SupportProject';
+import UploadProject from './Tab/UploagProject';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <div>{children}</div>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+  
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+const MyTabs = () => {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <>
+            <Box sx={{ width: '100%' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label="프로필" {...a11yProps(0)} />
+                        <Tab label="올린 프로젝트" {...a11yProps(1)} />
+                        <Tab label="후원한 프로젝트" {...a11yProps(2)} />
+                    </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+                    <ProfileTab />
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    <UploadProject/>   
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    <SupportProject/>
+                </TabPanel>
+            </Box>
+        </>
+    )
+
+
+}
+
+export default MyTabs
