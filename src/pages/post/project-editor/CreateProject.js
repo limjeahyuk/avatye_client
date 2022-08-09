f2168eeb11f4d70aa3403d0a5db8579ccc3445ba
leeeeeeeminji <ldfu0269@naver.com>
2022-08-09 16:26:13 +0900

    index on feature/2-basicInfoProject: e672247 basicinfo 여전히 작업중

--- src/pages/post/project-editor/CreateProject.js
+++ src/pages/post/project-editor/CreateProject.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import BasicInfo from "../../../components/post/createProject/BasicInfo";
 
 const CreateProject = () => {
+    const [basicData, setBasicData] = useState({});
+
+    const basicHandler = (data) => {
+        setBasicData(data);
+    }
+
     return <>
-    <BasicInfo />
+    <BasicInfo dataHandler={basicHandler} />
     </>
 }
 
