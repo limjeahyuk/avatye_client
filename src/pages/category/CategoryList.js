3f0ffdff3ce403dc96b996dd8f4fccbdcd9e0e80
Jsubak <tnwjd1003@gmail.com>
2022-08-22 16:47:03 +0900

    카테고리 추가

---
+++ src/pages/category/CategoryList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Category from "../../components/ui/category/Category";
+
+const CategoryList = () => {
+    return<><Category/></>
+}
+
+export default CategoryList
